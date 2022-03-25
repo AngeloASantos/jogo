0354e1579c6b50feae719f0bc2ed998fbb9b79db
AngeloASantos <angeloafonsosantos@gmail.com>
2022-03-25 20:02:16 -0300

    Trabalho com Orientação a objetos no php

--- conectaBanco.php
+++ conectaBanco.php
@@ -1,7 +1,19 @@
 <?php
 
-$conecta = mysqli_connect("localhost","root","","game");
-if(!$conecta){
-    echo "Erro";
-    exit;
+// $conecta = mysqli_connect("localhost","root","","game");
+// if(!$conecta){
+//     echo "Erro";
+//     exit;
+// }
+
+abstract class bancoDeDados{
+    private function_construct(){}
+
+    private function_clone(){}
+
+    public function_destruct(){
+        foreach($this as $key => $value){
+            unset($this->key);
+        }
+    }
 }
